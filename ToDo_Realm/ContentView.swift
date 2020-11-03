a018be139bb44e7f46b89d4addc89cbc61dfe875
Kevin <vokevin9@gmail.com>
2020-11-03 12:58:10 -0600

    Initial Commit

---
+++ ToDo_Realm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo_Realm
+//
+//  Created by Kevin Vo on 11/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDo_Realm/ToDo_RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_RealmApp.swift
+//  ToDo_Realm
+//
+//  Created by Kevin Vo on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
